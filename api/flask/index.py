4f47d59e509dd7d9783850b7d05791af79017f93
Jamie Young <jamie.young@fordfuels.co.uk>
2026-03-30 10:03:22 +0100

    Enable CORS for Flask API

--- api/flask/index.py
+++ api/flask/index.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from ytmusicapi import YTMusic
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/flask/search", methods=["GET"])
 def search():
@@ -16,24 +18,18 @@ def search():
 
     # Limit to 5 results
     results = results[:5]
-    # Loop over results to filter out songs that aren't embeddable
-    # TODO is there a better way to do this? 
-    # Maybe streaming the results back?
-    # In my rudimentary test a request took 1.17s without the filter, while it took 2.93s with filtering enabled.
-    # But on the other hand, we kind of *need* to check this, because otherwise we'll get songs that are not embeddable and will fail in the last second.
-    #results = [result for result in results if yt.get_song(result["videoId"])["playabilityStatus"].get("playableInEmbed", False)]
     return jsonify(results)
 
 @app.route("/flask/get-mood-categories", methods=["GET"])
 def get_mood_categories():
     yt = YTMusic()
     mood_categories = yt.get_mood_categories()
-    
+
     # Only show the "Moods & moments" category
     mood_categories = mood_categories["Moods & moments"]
 
     response = jsonify(mood_categories)
-    response.cache_control.max_age = 60 * 60 * 24 # 24 hours
+    response.cache_control.max_age = 60 * 60 * 24  # 24 hours
     return response
 
 @app.route("/flask/get-mood-playlists", methods=["GET"])
@@ -47,7 +43,7 @@ def get_mood_playlists():
     # Limit to 15 results
     playlists = playlists[:15]
     response = jsonify(playlists)
-    response.cache_control.max_age = 60 * 60 * 24 # 24 hours
+    response.cache_control.max_age = 60 * 60 * 24  # 24 hours
     return response
 
 @app.route("/flask/get-playlist", methods=["GET"])
