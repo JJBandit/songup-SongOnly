2b6fae650e0c0b952b795ff2574959da84fa8219
jamie Young <jayounggames@gmail.com>
2026-05-01 16:19:21 +0100

    merged flask API into next.js route handlers

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
     rewrites: async () => {
         return [
-            {
-                source: "/flask/:path*",
-                destination:
-                    process.env.NODE_ENV === "development"
-                        ? "http://127.0.0.1:5328/flask/:path*"
-                        : "/api/flask/index",
-            },
+            
             {
                 source: "/relay-iljT/static/:path*",
                 destination: "https://eu-assets.i.posthog.com/static/:path*",
